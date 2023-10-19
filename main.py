7545cdacf6797eab422c1185ae1a422f3a73c76e
Pavan N R <111444726+PavanNR958@users.noreply.github.com>
2023-10-19 22:18:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import qrcode
+
+url = input("Enter the URL: ")
+
+qr = qrcode.QRCode(
+    version=1, 
+    error_correction=qrcode.constants.ERROR_CORRECT_L, 
+    box_size=10,  
+    border=4,  
+)
+
+qr.add_data(url)
+
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="white", back_color="black")
+
+img.save("output.png")
